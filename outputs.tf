728dc6e2af65facfad941cf8510a2e6bc51470dd
Daniel Allen <dan@aeternagroup.net>
2026-01-11 16:54:11 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "network_manager_security_admin_configurations" {
+  description = "All network_manager_security_admin_configuration resources"
+  value       = azurerm_network_manager_security_admin_configuration.network_manager_security_admin_configurations
+}
+
